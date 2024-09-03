cf26e3fb67eb6fd86f752165cb58b647913b7596
Saray Martinez <sm78544@lasallebajio.edu.mx>
2024-09-03 08:36:22 -0600

    pr5

---
+++ practica5/js/index.js
@@ -0,0 +1,12 @@
+var miVar = "Hola mundo";
+console.log(miVar);
+var per1 = {
+    firstName1: "joan",
+    lastName1: "doe",
+    age: 50
+};
+var firstName1 = per1.firstName1, lastName1 = per1.lastName1;
+console.log(firstName1, ' ', lastName1);
+var miNom = "Marc";
+var a1 = miNom[0], a2 = miNom[1], a3 = miNom[2], a4 = miNom[3], a5 = miNom[4];
+console.log(a1, a2, a3, a4, a5);
---
+++ practica5/src/index.ts
@@ -0,0 +1,18 @@
+let miVar:string = "Hola mundo";
+console.log(miVar);
+
+
+
+const per1 = {
+    firstName1:"joan",
+    lastName1:"doe",
+    age: 50
+};
+
+
+let {firstName1, lastName1} =per1;
+console.log(firstName1, ' ', lastName1);
+
+let miNom = "Marc";
+let [a1, a2, a3, a4, a5]:any = miNom;
+console.log(a1, a2, a3, a4, a5);

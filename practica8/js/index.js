472476c4df4ec40fd80cb80ba7c684f35114f487
Saray Martinez <sm78544@lasallebajio.edu.mx>
2024-09-24 08:23:42 -0600

    pr8

--- practica8/js/index.js
+++ practica8/js/index.js
@@ -1,2 +1,29 @@
-"use strict";
 console.log("hola codespace");
+var nuevoHeroe = 'CHapulin colorado';
+function returnName() {
+    return nuevoHeroe;
+}
+var heroeNombre = returnName();
+var otraVariable = returnName();
+console.log('De que tipo es: ', typeof otraVariable);
+//Parametros obligatorios en funciones
+var nombreCompleto = function (nombre, apellido) {
+    return "".concat(nombre, " ").concat(apellido);
+};
+var tuNombre = nombreCompleto("Carlos", "Serano");
+console.log(tuNombre);
+//Parametros opcionales en funciones
+var nombreCompleto2 = function (nombre, apellido) {
+    return "".concat(nombre, " ").concat(apellido || 'sin apellido');
+};
+var tuNombre2 = nombreCompleto2("Carlos2");
+console.log(tuNombre2);
+var nombreCompleto3 = function (nombre, apellido, mayusculas) {
+    if (mayusculas === void 0) { mayusculas = false; }
+    if (mayusculas) {
+        return "".concat(nombre || 'sin nombre', " ").concat(apellido || 'sin apellido').toUpperCase();
+    }
+    return "".concat(nombre || 'sin nombre', " ").concat(apellido || 'sin apellido');
+};
+var tuNombre3 = nombreCompleto3("saray", "martinez", true);
+console.log(tuNombre3);
--- practica8/src/index.ts
+++ practica8/src/index.ts
@@ -1,2 +1,45 @@
 console.log("hola codespace");
 
+const nuevoHeroe:string = 'CHapulin colorado';
+function returnName():string {
+    return nuevoHeroe;
+}
+const heroeNombre = returnName();
+let otraVariable = returnName();
+console.log('De que tipo es: ', typeof otraVariable);
+
+//Parametros obligatorios en funciones
+const nombreCompleto = (nombre:string, apellido:string) =>{
+    return `${nombre} ${apellido}`;
+}
+
+const tuNombre = nombreCompleto("Carlos", "Serano");
+console.log(tuNombre);
+
+//Parametros opcionales en funciones
+
+const nombreCompleto2 = (nombre:string, apellido?:string) =>{
+    return `${nombre} ${apellido || 'sin apellido'}`;
+}
+
+const tuNombre2 = nombreCompleto2("Carlos2");
+console.log(tuNombre2);
+
+const nombreCompleto3 = (nombre?: string, apellido?: string, mayusculas: boolean = false) => {
+
+    if (mayusculas) {
+        return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase();
+    }
+    return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
+}
+
+const tuNombre3 = nombreCompleto3("saray", "martinez", true);
+console.log(tuNombre3);
+
+const nombreCompleto4 = (nombre: string, edad: number, mayusculas: boolean = false) => {
+
+    return `${nombre || 'sin nombre'} ${edad || 'sin apellido'}`;
+}
+
+const tuNombre4 = nombreCompleto4("saray", 22, true);
+console.log(tuNombre4);
